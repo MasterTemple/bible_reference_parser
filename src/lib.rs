7eea1b489f0803494f40854a104e5c148094ad3e
Blake Scampone <54373793+MasterTemple@users.noreply.github.com>
2025-03-10 19:00:00 -0700

    removed overlap file

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,7 @@
 pub mod compare;
 pub mod maps;
-pub mod overlap;
 pub mod parse;
 pub mod passage;
 pub mod passage_segments;
 pub mod segment;
 pub mod segments;
-
-// #[cfg(test)]
-// mod test {
-//
-//     #[test]
-//     fn main() {
-//         // run code here
-//     }
-// }
