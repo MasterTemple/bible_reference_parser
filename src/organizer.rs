aa943293b28c7f8940b38ec006127d3d25aca7f2
Blake Scampone <54373793+MasterTemple@users.noreply.github.com>
2025-03-10 19:00:00 -0700

    changed chapter_range key type

--- src/organizer.rs
+++ src/organizer.rs
@@ -19,11 +19,11 @@ pub struct BookOrganizer<Container: Debug + Default> {
     /// `map[chapter][start_verse][end_verse] -> Container`
     chapter_verse_range: BTreeMap<u8, BTreeMap<(u8, u8), Container>>,
     /// `map[start_chapter][start_verse][end_chapter][end_verse] -> Container`
-    chapter_range: BTreeMap<u8, BTreeMap<u8, BTreeMap<u8, BTreeMap<u8, Container>>>>,
+    // chapter_range: BTreeMap<u8, BTreeMap<u8, BTreeMap<u8, BTreeMap<u8, Container>>>>,
+    chapter_range: BTreeMap<(u8, u8), BTreeMap<(u8, u8), Container>>,
     /// `map[chapter] -> Container`
     full_chapter: BTreeMap<u8, Container>,
     /// `map[start_chapter][end_chapter] -> Container`
-    // full_chapter_range: BTreeMap<u8, BTreeMap<u8, Container>>,
     full_chapter_range: BTreeMap<(u8, u8), Container>,
 }
 
@@ -62,10 +62,12 @@ impl<Container: Debug + Default> BookOrganizer<Container> {
                     .entry((seg.verses.start, seg.verses.end)).or_default()
             },
             PassageSegment::ChapterRange(seg) => {
-                self.chapter_range.entry(seg.start.chapter).or_default()
-                    .entry(seg.start.verse).or_default()
-                    .entry(seg.end.chapter).or_default()
-                    .entry(seg.end.verse).or_default()
+                // self.chapter_range.entry(seg.start.chapter).or_default()
+                //     .entry(seg.start.verse).or_default()
+                //     .entry(seg.end.chapter).or_default()
+                //     .entry(seg.end.verse).or_default()
+                self.chapter_range.entry((seg.start.chapter, seg.end.chapter)).or_default()
+                    .entry((seg.start.verse, seg.end.verse)).or_default()
             },
             PassageSegment::FullChapter(seg) => {
                 self.full_chapter.entry(seg.chapter).or_default()
@@ -75,6 +77,10 @@ impl<Container: Debug + Default> BookOrganizer<Container> {
             },
         }
     }
+
+    // pub fn get_all_content<'a>(&'a self) -> impl Iterator<Item = (PassageSegment, &'a Container)> {
+    //     todo!()
+    // }
 }
 
 
@@ -85,20 +91,6 @@ impl<Container: Debug + Default> BookOrganizer<Container> {
                 .map(move|(&verse, container)| (ChapterVerse::new(chapter, verse), container))
         })
     }
-
-    // pub fn get_chapter_verse_range_content<'a>(&'a self, seg: &'a impl SegmentCompare) -> impl Iterator<Item = (ChapterVerseRange, &'a Container)> {
-    //     self.chapter_verse_range.range(seg.chapter_range()).flat_map(|(&chapter, verse_range_map)| {
-    //         let verse_range = seg.verse_range(chapter);
-    //         verse_range_map.range(verse_range).flat_map(move|(&start_verse, map)| {
-    //             // Are you sure `verse_range` should not be `start_verse..=seg.ending_verse()`?
-    //             // But start verse will always be 1 except for the first time, which is covered by
-    //             // the `verse_range` method
-    //             map.range(verse_range).map(move|(&end_verse, container)| {
-    //                 (ChapterVerseRange::new(chapter, start_verse, end_verse), container)
-    //             })
-    //         })
-    //     })
-    // }
 
     pub fn get_chapter_verse_range_content<'a>(&'a self, key: &'a impl SegmentCompare) -> impl Iterator<Item = (ChapterVerseRange, &'a Container)> {
         self.chapter_verse_range.range(key.chapter_range()).flat_map(move |(&chapter, verse_range_map)| {
@@ -113,20 +105,34 @@ impl<Container: Debug + Default> BookOrganizer<Container> {
     }
 
 
-    pub fn get_chapter_range_content<'a>(&'a self, seg: &'a impl SegmentCompare) -> impl Iterator<Item = (ChapterRange, &'a Container)> {
-         self.chapter_range.range(seg.chapter_range()).flat_map(move|(&start_chapter, map1)| {
-            map1.range(seg.verse_range(start_chapter)).flat_map(move|(&start_verse, ending_chapter_map)| {
-                ending_chapter_map.range(seg.chapter_range()).flat_map(move|(&end_chapter, ending_verse_map)| {
-                    ending_verse_map.range(seg.verse_range(end_chapter)).map(move|(&end_verse, container)| {
-                        (ChapterRange::new(start_chapter, start_verse, end_chapter, end_verse), container)
-                    })
-                })
+    // pub fn get_chapter_range_content<'a>(&'a self, seg: &'a impl SegmentCompare) -> impl Iterator<Item = (ChapterRange, &'a Container)> {
+    //      self.chapter_range.range(seg.chapter_range()).flat_map(move|(&start_chapter, map1)| {
+    //         map1.range(seg.verse_range(start_chapter)).flat_map(move|(&start_verse, ending_chapter_map)| {
+    //             ending_chapter_map.range(seg.chapter_range()).flat_map(move|(&end_chapter, ending_verse_map)| {
+    //                 ending_verse_map.range(seg.verse_range(end_chapter)).map(move|(&end_verse, container)| {
+    //                     (ChapterRange::new(start_chapter, start_verse, end_chapter, end_verse), container)
+    //                 })
+    //             })
+    //         })
+    //     })
+    // }
+
+    pub fn get_chapter_range_content<'a>(&'a self, key: &'a impl SegmentCompare) -> impl Iterator<Item = (ChapterRange, &'a Container)> {
+         self.chapter_range.iter().flat_map(move|(&(start_chapter, end_chapter), verse_range_map)| {
+            // I just do `iter` because I need to start from the beginning of a range because I dont know when it ends
+            verse_range_map.iter().filter_map(move|(&(start_verse, end_verse), container)| {
+                let seg = ChapterRange::new(start_chapter, start_verse, end_chapter, end_verse);
+                seg.overlaps_with(key).then(|| (seg, container))
             })
+            // early terminate when the key ends before the start of this segment
+            .take_while(|(seg, _)| !key.ends_before(seg))
         })
+        // early terminate when the key ends before the start of this segment
+        .take_while(|(seg, _)| !key.ends_before(seg))
     }
 
-    pub fn get_full_chapter_content(&self, seg: &impl SegmentCompare) -> impl Iterator<Item = (FullChapter, &Container)> {
-        self.full_chapter.range(seg.chapter_range())
+    pub fn get_full_chapter_content(&self, key: &impl SegmentCompare) -> impl Iterator<Item = (FullChapter, &Container)> {
+        self.full_chapter.range(key.chapter_range())
             .map(|(&chapter, container)| (FullChapter::new(chapter), container))
     }
 
@@ -197,6 +203,35 @@ mod tests {
 
         assert_eq!(org.get_chapter_verse_range_content(&FullChapterRange::new(1, 2)).count(), 3);
         assert_eq!(org.get_chapter_verse_range_content(&FullChapterRange::new(4, 5)).count(), 0);
+    }
+
+    #[test]
+    fn chapter_range() {
+        let mut org = BookOrganizer::<()>::new();
+        org.modify(ChapterRange::new(2, 1, 3, 3));
+        org.modify(ChapterRange::new(2, 4, 3, 7));
+        org.modify(ChapterRange::new(1, 1, 4, 3));
+
+        assert_eq!(org.get_chapter_range_content(&ChapterVerse::new(2, 1)).count(), 2);
+        assert_eq!(org.get_chapter_range_content(&ChapterVerse::new(2, 4)).count(), 3);
+        assert_eq!(org.get_chapter_range_content(&ChapterVerse::new(4, 1)).count(), 1);
+        assert_eq!(org.get_chapter_range_content(&ChapterVerse::new(5, 1)).count(), 0);
+
+        assert_eq!(org.get_chapter_range_content(&ChapterVerseRange::new(2, 1, 4)).count(), 3);
+        assert_eq!(org.get_chapter_range_content(&ChapterVerseRange::new(2, 5, 8)).count(), 3);
+        assert_eq!(org.get_chapter_range_content(&ChapterVerseRange::new(4, 1, 2)).count(), 1);
+        assert_eq!(org.get_chapter_range_content(&ChapterVerseRange::new(5, 1, 2)).count(), 0);
+
+        assert_eq!(org.get_chapter_range_content(&ChapterRange::new(1, 1, 2, 2)).count(), 2);
+        assert_eq!(org.get_chapter_range_content(&ChapterRange::new(4, 1, 5, 1)).count(), 1);
+        assert_eq!(org.get_chapter_range_content(&ChapterRange::new(4, 4, 5, 1)).count(), 0);
+
+        assert_eq!(org.get_chapter_range_content(&FullChapter::new(2)).count(), 3);
+        assert_eq!(org.get_chapter_range_content(&FullChapter::new(4)).count(), 1);
+        assert_eq!(org.get_chapter_range_content(&FullChapter::new(5)).count(), 0);
+
+        assert_eq!(org.get_chapter_range_content(&FullChapterRange::new(1, 2)).count(), 3);
+        assert_eq!(org.get_chapter_range_content(&FullChapterRange::new(4, 5)).count(), 1);
     }
 
     #[test]
