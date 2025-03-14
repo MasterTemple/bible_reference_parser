fda45c5d637fe6f3544b1b3fff41cf2d3d4ad54a
Blake Scampone <54373793+MasterTemple@users.noreply.github.com>
2025-03-13 19:03:06 -0700

    initial work on iterator for book passage segments

--- src/segments.rs
+++ src/segments.rs
@@ -2,15 +2,40 @@ use std::{fmt::Debug, ops::{Deref, DerefMut}};
 
 use serde::{Deserialize, Serialize};
 
-use crate::{compare::SegmentCompare, segment::PassageSegment};
+use crate::{book_segment::BookSegment, compare::SegmentCompare, segment::PassageSegment};
 
+/// todo: create method to iter segments as book segments
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct BookPassageSegments {
     book: u8,
     segments: PassageSegments,
 }
 
+// impl IntoIterator for &BookPassageSegments {
+//     type Item;
+//
+//     type IntoIter;
+//
+//     fn into_iter(self) -> Self::IntoIter {
+//         todo!()
+//     }
+// }
+
+// impl Iterator for BookPassageSegments {
+//     type Item = BookSegment<PassageSegment>;
+//
+//     fn next(&mut self) -> Option<Self::Item> {
+//         self.iter()
+//     }
+// }
+
+
 impl BookPassageSegments {
+    pub fn iter(&self) -> impl Iterator<Item = BookSegment<PassageSegment>> + '_ {
+        let book = self.book;
+        self.segments.iter().map(move |seg| seg.with_book(book))
+    }
+
     pub fn parse(book: u8, segment_input: &str) -> Result<Self, String> {
         Ok(Self {
             book,
@@ -46,6 +71,13 @@ impl PassageSegments {
         Self(vec![])
     }
 
+    pub fn with_book(self, book: u8) -> BookPassageSegments {
+        BookPassageSegments {
+            book,
+            segments: self
+        }
+    }
+
     // pub fn overlaps_segment(&self, other: impl Into<PassageSegment>) -> bool {
     pub fn overlaps_with(&self, other: &impl SegmentCompare) -> bool {
         self.iter().any(|this| this.overlaps_with(other))
@@ -55,5 +87,18 @@ impl PassageSegments {
     /// - I just need some way to order the segments and do it in linear time
     pub fn contains_overlap(&self, other: &PassageSegments) -> bool {
         self.iter().any(|this| other.overlaps_with(this))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BookPassageSegments;
+
+    #[test]
+    fn test() {
+        let segs = BookPassageSegments::parse(1, "1:1,3-4").unwrap();
+        for seg in segs.iter() {
+            println!("{seg:#?}");
+        }
     }
 }
